d6c7f0c5015699ed35ecca8d4235bf6cd4de4983
abhaynm02 <abhaybhaskarnm02@gmail.com>
2024-08-21 16:59:29 +0530

    set-up the project

---
+++ src/main/java/com/example/e_commerce_OAuth2/ECommerceOAuth2Application.java
@@ -0,0 +1,13 @@
+package com.example.e_commerce_OAuth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceOAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceOAuth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/e_commerce_OAuth2/ECommerceOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_commerce_OAuth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceOAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
